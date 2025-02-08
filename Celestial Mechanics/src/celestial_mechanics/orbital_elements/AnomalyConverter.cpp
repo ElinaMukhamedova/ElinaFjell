161d290c684822421bb182d5472f1451418e427b
Elina <elinamuhamedova13@gmail.com>
2025-02-08 07:48:27 +0300

    MeanToEccentric

--- Celestial Mechanics/src/celestial_mechanics/orbital_elements/AnomalyConverter.cpp
+++ Celestial Mechanics/src/celestial_mechanics/orbital_elements/AnomalyConverter.cpp
@@ -24,17 +24,15 @@ double EccentricToMean(double const& eccentricity, double const& eccentricAnomal
 double MeanToEccentric(double const& eccentricity, double const& meanAnomaly) {
     double const accuracy = 1000 * std::numeric_limits<double>::epsilon();
     int const maxIter = 50;
-    bool solutionFound = false;
     int iter = 0;
     double E_prev = meanAnomaly>std::numbers::pi ?
                             meanAnomaly-eccentricity : meanAnomaly+eccentricity;
     double E;
-    while (solutionFound == false && iter < maxIter) {
+    while (iter < maxIter) {
         E = E_prev - (E_prev-eccentricity*std::sin(E_prev)-meanAnomaly)/(1-eccentricity*std::cos(E_prev));
-        if (std::abs(E-E_prev) <= accuracy) {solutionFound = true;}
-        E_prev = E;
         ++iter;
+        if (std::abs(E-E_prev) <= accuracy) {return E;}
+        E_prev = E;
     }
-    if (solutionFound == false) {throw Exception("Kepler solver failed to converge!");}
-    return E;
+    throw Exception("Kepler solver failed to converge!");
 }
