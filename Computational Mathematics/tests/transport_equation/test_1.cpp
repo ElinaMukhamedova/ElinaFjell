cfd7a17f8e999dd909b2d7e59051a24247e5acab
Elina <elinamuhamedova13@gmail.com>
2025-02-12 19:18:07 +0300

    GridTest

--- Computational Mathematics/tests/transport_equation/test_1.cpp
+++ Computational Mathematics/tests/transport_equation/test_1.cpp
@@ -9,5 +9,10 @@ TEST(GridTest, GridStaticMembersWork) {
 }
 
 TEST(GridTest, GridConstructorWorks) {
-    
+    Grid myGrid{0.7};
+    ASSERT_EQ(myGrid.Courant, 0.7);
+    ASSERT_EQ(myGrid.h_t, 0.35);
+    ASSERT_EQ(myGrid.L, 20);
+    ASSERT_EQ(myGrid.T, 18);
+    ASSERT_EQ(myGrid.h_x, 0.5);
 }
